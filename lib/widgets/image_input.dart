56303ccb9a4313810c59078234d1ce92527467c2
Habibur Rahman <mhr.habibur@gmail.com>
2025-05-01 12:03:14 +0600

    Image picker is added and now user can pick image with camera.

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,8 +10,45 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _imageFile;
+
+  void _takeImage() async {
+    final ImagePicker picker = ImagePicker();
+    final pickedImage = await picker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+    if (pickedImage == null) {
+      return;
+    }
+
+    setState(() {
+      _imageFile = File(pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+      onPressed: () {
+        _takeImage();
+      },
+      label: Text("Take Picture"),
+      icon: Icon(Icons.camera_alt),
+    );
+
+    if (_imageFile != null) {
+      content = GestureDetector(
+        onTap: _takeImage,
+        child: Image.file(
+          _imageFile!,
+          fit: BoxFit.cover,
+          height: double.infinity,
+          width: double.infinity,
+        ),
+      );
+    }
+
     return Container(
       decoration: BoxDecoration(
         border: Border.all(width: 1, color: Colors.black),
@@ -17,11 +56,7 @@ class _ImageInputState extends State<ImageInput> {
       height: 250,
       width: double.infinity,
       alignment: Alignment.center,
-      child: TextButton.icon(
-        onPressed: () {},
-        label: Text("Take Picture"),
-        icon: Icon(Icons.camera_alt),
-      ),
+      child: content,
     );
   }
 }
