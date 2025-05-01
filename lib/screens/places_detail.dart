63327c5f909baed6ad1444618ab6031985ac068d
Habibur Rahman <mhr.habibur@gmail.com>
2025-05-01 15:28:12 +0600

    Image is added in place detail page.

--- lib/screens/places_detail.dart
+++ lib/screens/places_detail.dart
@@ -9,8 +9,25 @@ class PlacesDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(place.title)),
-      body: Center(child: Text(place.title)),
+      appBar: AppBar(title: Text("Your favorite place")),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            Text(place.title, style: TextStyle(fontSize: 20)),
+            SizedBox(height: 16),
+            Container(
+              height: 250,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+              ),
+              child: Image.file(place.image, fit: BoxFit.cover),
+            ),
+            SizedBox(height: 16),
+          ],
+        ),
+      ),
     );
   }
 }
